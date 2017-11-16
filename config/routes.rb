941c0b07c4aaffeae744a99ac17b6be6a2334fd9
hugosykes <hugo@denmeadfarm.co.uk>
2017-11-16 21:12:12 +0000

    added comments model and the references: user -> pictures, pictures - > comments, user -> comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :pictures
   devise_for :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116180330) do
+ActiveRecord::Schema.define(version: 20171116211106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "picture_id"
+    t.bigint "user_id"
+    t.index ["picture_id"], name: "index_comments_on_picture_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string "name"
@@ -42,5 +52,7 @@ ActiveRecord::Schema.define(version: 20171116180330) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "pictures"
+  add_foreign_key "comments", "users"
   add_foreign_key "pictures", "users"
 end
