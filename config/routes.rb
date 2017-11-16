eb405b6bf562775bed2547c0d5251c877da73758
hugosykes <hugo@denmeadfarm.co.uk>
2017-11-16 21:01:24 +0000

    removed welcome page in favour of pictures index and altered layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :pictures
   devise_for :users
 
-  root to: 'welcome#index'  
+  root to: 'pictures#index'  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
