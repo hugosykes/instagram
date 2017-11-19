c9f8f874b72f8f498df6f139db3f75d12de04a96
hugosykes <hugo@denmeadfarm.co.uk>
2017-11-19 13:33:29 +0000

    Updated redirect path of the update method in the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,7 +43,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to "/pictures/#{params[:picture_id]}", notice: 'Comment was successfully updated.' }
+        format.html { redirect_to picture_url(params[:picture_id]), notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
